6c4da61f02db027a52a0e314952d962b8085369f
Ezia <esia.belbachir@protonmail.com>
2018-03-21 19:33:15 -0400

    Implement dice sum test

--- src/RandomNumberGenerator.py
+++ src/RandomNumberGenerator.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 import math
+from decimal import *
+
+getcontext().prec = 2
 
 class RandomNumberGenerator:
     """A linear congruential generator"""
@@ -33,22 +36,60 @@ class NumericalRecipesGenerator(RandomNumberGenerator):
     def __init__(self, seed):
         RandomNumberGenerator.__init__(self, 2**32, 1664525, 1013904223, seed, 0, 31)
 
-def simpleRngTest(a, b, interationNbr, generator):
+def simpleRngTest(a, b, iterationNbr, generator):
     """Test min, max and average value of a generator"""
 
     minValue = b
     maxValue = a
     sumValues = 0
 
-    for i in range(interationNbr):
+    for i in range(iterationNbr):
         randomNumber = generator.next(a, b)
         minValue = min(minValue, randomNumber)
         maxValue = max(maxValue, randomNumber)
         sumValues = sumValues + randomNumber
 
     print("param : minValue = " + str(a) + " ; maxValue = " + str(b) 
-            + " ; iteration number = " + str(interationNbr))
+            + " ; iteration number = " + str(iterationNbr))
     print("results : minValue = " + str(minValue) + " ; maxValue = " + str(maxValue) 
-            + " ; average = " + str(sumValues/interationNbr))
+            + " ; average = " + str(sumValues/iterationNbr))
 
-simpleRngTest(-2, 2, 10000, NumericalRecipesGenerator(1))
+def diceTest(iterationNbr):
+    generator = BordlandGenerator(6)
+
+    # expected sum probability from 2 to 12
+    expectedProba = [1/36, 1/18, 1/12, 1/9, 5/36, 1/6, 5/36, 1/9, 1/12, 1/18, 1/36]
+
+    # empirical sum statistics from 2 to 12
+    empiricalStat = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+    # Run dice throw
+    for i in range(iterationNbr):
+        index = int((generator.next(1, 7)) + int(generator.next(1, 7)))-2
+        empiricalStat[index] = empiricalStat[index] + 1
+    
+    # Compute statistics
+
+    # Khi 2 statistic with 11 categories
+    T1 = 0
+    for i in range(11):
+        # empirical statistics
+        empiricalStat[i] = empiricalStat[i] / iterationNbr
+        # Compute khi 2 statistic
+        T1 = T1 + (iterationNbr * empiricalStat[i] - iterationNbr * expectedProba[i])**2/(iterationNbr * expectedProba[i]) 
+
+    # Khi 2 statistic with {2, 3} and {11, 12} values gathered (9 categories)
+    T2 = 0
+    T2 = T2 + (iterationNbr * (empiricalStat[0]+empiricalStat[1]) - iterationNbr * (expectedProba[0]+expectedProba[1]))**2/(iterationNbr * (expectedProba[0]+expectedProba[1])) 
+    T2 = T2 + (iterationNbr * (empiricalStat[9]+empiricalStat[10]) - iterationNbr * (expectedProba[9]+expectedProba[10]))**2/(iterationNbr * (expectedProba[9]+expectedProba[10])) 
+    for i in range(2, 9):
+        T2 = T2 + (iterationNbr * empiricalStat[i] - iterationNbr * expectedProba[i])**2/(iterationNbr * expectedProba[i]) 
+
+    print("expected probabilities : " + str(["%.4f" % e for e in expectedProba]))
+    print("empirical statistics :   " + str(["%.4f" % e for e in empiricalStat]))
+    print("khi 2 statistic : " + str(T1) + " ||| khi 2 law with 5% risk of degree 10 = " + str(18.31))
+    print("khi 2 statistic (gathered extremities) : " + str(T2) + " ||| khi 2 law with 5% risk of degree 8 = " + str(15.51))
+
+
+#simpleRngTest(-2, 2, 10000, NumericalRecipesGenerator(1))
+diceTest(1000)
